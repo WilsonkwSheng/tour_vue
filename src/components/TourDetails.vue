ca2117d5e26404ed2d98bea855b19836f2bad5af
Wilson <imweesheng@gmail.com>
2024-03-30 13:51:53 +0800

    Add basic styling to tour details page

--- src/components/TourDetails.vue
+++ src/components/TourDetails.vue
@@ -1,39 +1,40 @@
 <template>
-  <div v-if="tour">
-    <div v-for="itinerary in tour.itineraries" :key="itinerary.id">
-      <div v-if="itinerary.images.length > 0">
-        <div class="swiper">
-          <div class="swiper-wrapper">
-            <div class="swiper-slide" v-for="image in itinerary.images" :key="image.id">
-              <img class="swiper-slide" :src="'http://localhost:3000/' + image.file.url" alt="Itinerary Image">
+  <div class="bg-white shadow-md rounded-lg p-6 md:p-10">
+    <div v-if="tour">
+      <p class="text-lg font-semibold">Region: {{ tour.region }}</p>
+      <p class="text-lg font-semibold">City: {{ tour.city }}</p>
+      <p class="text-lg font-semibold">Travel Type: {{ tour.travel_type }}</p>
+    </div>
+    <div>
+      <div v-if="tour">
+        <h1 class="text-3xl font-bold mt-8 mb-4">{{ tour.title }}</h1>
+        <p class="text-lg">{{ tour.description }}</p>
+        <h4 class="text-xl font-semibold mt-6 mb-2">Itinerary</h4>
+        <div v-for="itinerary in tour.itineraries" :key="itinerary.id" class="mb-8 flex items-center">
+          <div v-if="itinerary.images.length > 0" class="mr-4">
+            <div class="swiper">
+              <div class="swiper-wrapper">
+                <div class="swiper-slide" v-for="image in itinerary.images" :key="image.id">
+                  <img class="swiper-slide w-full" :src="'http://localhost:3000/' + image.file.url" alt="Itinerary Image">
+                </div>
+              </div>
+              <div class="swiper-pagination"></div>
+              <div class="swiper-button-prev"></div>
+              <div class="swiper-button-next"></div>
             </div>
           </div>
-          <div class="swiper-pagination"></div>
-          <div class="swiper-button-prev"></div>
-          <div class="swiper-button-next"></div>
+          <div>
+            <h5 class="text-lg font-semibold">{{ itinerary.title }}</h5>
+            <p class="text-lg">{{ itinerary.description }}</p>
+            <p class="text-lg">Date: {{ itinerary.date }}</p>
+            <p class="text-lg">Start Time: {{ itinerary.start_at }}</p>
+            <p class="text-lg">End Time: {{ itinerary.end_at }}</p>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-  <div>
-    <h2>Tour Details</h2>
-    <div v-if="tour">
-      <h3>{{ tour.title }}</h3>
-      <p>{{ tour.description }}</p>
-      <p>Region: {{ tour.region }}</p>
-      <p>City: {{ tour.city }}</p>
-      <p>Travel Type: {{ tour.travel_type }}</p>
-      <h4>Itineraries</h4>
-      <div v-for="itinerary in tour.itineraries" :key="itinerary.id">
-        <h5>{{ itinerary.title }}</h5>
-        <p>{{ itinerary.description }}</p>
-        <p>Date: {{ itinerary.date }}</p>
-        <p>Start Time: {{ itinerary.start_at }}</p>
-        <p>End Time: {{ itinerary.end_at }}</p>
+      <div v-else>
+        <p class="text-lg">Loading...</p>
       </div>
-    </div>
-    <div v-else>
-      <p>Loading...</p>
     </div>
   </div>
 </template>
@@ -43,6 +44,7 @@ import axios from 'axios';
 import Swiper from 'swiper';
 import { Navigation, Pagination } from 'swiper/modules';
 import 'swiper/swiper-bundle.css';
+import '../index.css'
 
 export default {
   props: ['id'],
@@ -89,4 +91,8 @@ export default {
 </script>
 
 <style scoped>
+.swiper {
+  max-width: 300px;
+}
+
 </style>
