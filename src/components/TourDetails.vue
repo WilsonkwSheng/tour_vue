c761a31b86b5fbc505383c6d2102217507f4d635
Wilson <imweesheng@gmail.com>
2024-03-30 10:49:53 +0800

    Add itineraries and images details from updated endpoint for tour details page

--- src/components/TourDetails.vue
+++ src/components/TourDetails.vue
@@ -2,11 +2,28 @@
   <div>
     <h2>Tour Details</h2>
     <div v-if="tour">
-      <p>{{ tour.title }}</p>
+      <h3>{{ tour.title }}</h3>
       <p>{{ tour.description }}</p>
-      <p>{{ tour.region }}</p>
-      <p>{{ tour.city }}</p>
-      <p>{{ tour.travel_type }}</p>
+      <p>Region: {{ tour.region }}</p>
+      <p>City: {{ tour.city }}</p>
+      <p>Travel Type: {{ tour.travel_type }}</p>
+      <h4>Itineraries</h4>
+      <div v-for="itinerary in tour.itineraries" :key="itinerary.id">
+        <h5>{{ itinerary.title }}</h5>
+        <p>{{ itinerary.description }}</p>
+        <p>Date: {{ itinerary.date }}</p>
+        <p>Start Time: {{ itinerary.start_at }}</p>
+        <p>End Time: {{ itinerary.end_at }}</p>
+        <div v-if="itinerary.images.length > 0">
+          <h6>Images</h6>
+          <div v-for="image in itinerary.images" :key="image.id">
+            <img :src="'http://localhost:3000/' + image.file.url" alt="Itinerary Image">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div v-else>
+      <p>Loading...</p>
     </div>
   </div>
 </template>
